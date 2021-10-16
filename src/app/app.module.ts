076bd3762f2e70dba7bcd0bc95b912936f413fad
Andriy Bats <abats333@gmail.com>
2021-10-16 23:32:13 +0300

    second  commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { AdminBlogComponent } from './admin/admin-blog/admin-blog.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminCategoryComponent } from './admin/admin-category/admin-category.component';
+import { AdminProductComponent } from './admin/admin-product/admin-product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlogComponent,
+    AdminBlogComponent,
+    HeaderComponent,
+    AdminComponent,
+    AdminCategoryComponent,
+    AdminProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
